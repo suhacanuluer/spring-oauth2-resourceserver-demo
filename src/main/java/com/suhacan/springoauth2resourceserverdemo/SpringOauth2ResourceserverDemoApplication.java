d143a7f2d0370150c277c98db4aac4038c8dc3d7
Suha Can Uluer <sculuer@gmail.com>
2023-06-05 15:51:42 +0300

    Initial commit

---
+++ src/main/java/com/suhacan/springoauth2resourceserverdemo/SpringOauth2ResourceserverDemoApplication.java
@@ -0,0 +1,13 @@
+package com.suhacan.springoauth2resourceserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ResourceserverDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ResourceserverDemoApplication.class, args);
+	}
+
+}
